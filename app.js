ca3d9e67ad63b67370bf7a4400667dadb6414989
Stephanie Burlinchon <stephanieb170@gmail.com>
2023-03-14 22:47:37 +0000

    add shop items via data.js and app.js

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
+let shop = document.getElementById("shop");
+let basket = JSON.parse(localStorage.getItem("data")) || [];
 
+let generateShop = () => {
+  return (shop.innerHTML = shopItemsData
+    .map((x) => {
+      let { id, name, desc, img, price } = x;
+      let search = basket.find((y) => y.id === id) || [];
+      return `
+    <div id=product-id-${id} class="item">
+      <img width="220" src=${img} alt="">
+      <div class="details">
+        <h3>${name}</h3>
+        <p>${desc}</p>
+        <div class="price-quantity">
+          <h2>$ ${price} </h2>
+          <div class="buttons">
+            <i onclick="decrement(${id})" class="minus">-</i>
+            <div id=${id} class="quantity">${
+        search.item === undefined ? 0 : search.item
+      }</div>
+            <i onclick="increment(${id})" class="plus">+</i>
+          </div>
+        </div>
+      </div>
+  </div>
+    `;
+    })
+    .join(""));
+};
 
-function loadCart() {
-    window.location.href = "cart.html";
-  }
-
-const cartBtn = document.getElementById("cart-btn");
-cartBtn.addEventListener("click", loadCart);
-
+generateShop();
