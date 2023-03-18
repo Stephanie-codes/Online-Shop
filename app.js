c231fff758ea372cb9c876a624b804e6b6c9e186
Stephanie Burlinchon <stephanieb170@gmail.com>
2023-03-18 18:51:28 +0000

    plus and minus buttons working to update quantity

--- app.js
+++ app.js
@@ -5,12 +5,11 @@ document.addEventListener("DOMContentLoaded", function() {
 let shop = document.getElementById("shop");
 let basket = JSON.parse(localStorage.getItem("data")) || [];
 
-
 let generateShop = () => {
   return (shop.innerHTML = shopItemsData.map((x) => {
-    let { id, name, desc, img, price } = x;
+    let { id, name, desc, img, price, quantity } = x;
     return `
-    <div id="product${id}" class="item">
+    <div id="${id}" class="item">
       <img src=${img} alt="${name} product image">
       <div class="details">
         <h3>${name}</h3>
@@ -19,7 +18,7 @@ let generateShop = () => {
           <p>£ ${price} GBP</p>
           <div class="buttons">
             <i class="minus">-</i>
-            <div class="quantity">0
+            <div class="quantity">${quantity}
             </div>
             <i class="plus">+</i>
           </div>
@@ -32,4 +31,32 @@ let generateShop = () => {
 };
 
 generateShop();
+
+let insertShopIntoDOM = () => {
+  let shopHTML = generateShop();
+  let shop = document.querySelector('#shop');
+  shop.innerHTML = shopHTML;
+};
+
+insertShopIntoDOM();
+
+
+shop.addEventListener('click', event => {
+  const button = event.target;
+  if (button.classList.contains('plus')) {
+    const itemDiv = button.closest('.item');
+    const quantitySpan = itemDiv.querySelector('.quantity');
+    const x = shopItemsData.find(x => x.id == itemDiv.id);
+    x.quantity++;
+    quantitySpan.textContent = x.quantity;
+  } else if (button.classList.contains('minus')) {
+    const itemDiv = button.closest('.item');
+    const quantitySpan = itemDiv.querySelector('.quantity');
+    const x = shopItemsData.find(x => x.id == itemDiv.id);
+    if (x.quantity > 1) {
+      x.quantity--;
+      quantitySpan.textContent = x.quantity;
+    }
+  }
+});
 
--- data.js
+++ data.js
@@ -5,6 +5,7 @@ let shopItemsData = [
       price: 3,
       desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
       img: "images/apples.jpg",
+      quantity: 0,
     },
     {
       id: "002",
@@ -12,6 +13,7 @@ let shopItemsData = [
       price: 2,
       desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
       img: "images/bananas.jpg",
+      quantity: 0,
     },
     {
       id: "003",
@@ -19,6 +21,7 @@ let shopItemsData = [
       price: 7,
       desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
       img: "images/blueberries.jpg",
+      quantity: 0,
     },
     {
       id: "004",
@@ -26,6 +29,7 @@ let shopItemsData = [
       price: 5,
       desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
       img: "images/grapes.jpg",
+      quantity: 0,
     },
     {
       id: "005",
@@ -33,6 +37,7 @@ let shopItemsData = [
       price: 4,
       desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
       img: "images/oranges.jpg",
+      quantity: 0,
     },
     {
       id: "006",
@@ -40,6 +45,7 @@ let shopItemsData = [
       price: 7,
       desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
       img: "images/peaches.jpg",
+      quantity: 0,
     },
     {
       id: "007",
@@ -47,6 +53,7 @@ let shopItemsData = [
       price: 9,
       desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
       img: "images/raspberries.jpg",
+      quantity: 0,
     },
     {
       id: "008",
@@ -54,6 +61,7 @@ let shopItemsData = [
       price: 8,
       desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
       img: "images/strawberries.jpg",
+      quantity: 0,
     },
     {
       id: "009",
@@ -61,5 +69,6 @@ let shopItemsData = [
       price: 14,
       desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
       img: "images/watermelons.jpg",
+      quantity: 0,
     },
   ];
